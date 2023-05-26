34cc3093c69252a65a950bc4a86fda6500ce54f8
Abcuders <Abcuders@gmail.com>
2023-05-26 14:36:54 +0800

    <feat&docs>适配windows&更新md

--- AutoAnimeMv.py
+++ AutoAnimeMv.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 #coding:utf-8
 from sys import argv
-from os import system
+from os import system,path,name
 from time import sleep
 from re import findall,search,sub,I
 
 def TitleMatch(VideoName):
     Season = '01' #定义初始剧季为1
     #匹配待去除
-    SubtitleGroupDate = ['字幕','Raws','sub','汉化','搬运','月新番','Airota','Comicat','DMHY','NC-Raws','ANi','LoliHouse','Sakurato','TSDM','LoveEcho','EMe','Sakura','SweetSub','AHU-SUB','VCB-Studio','GM-Team','MingY','cc动漫','推しの子','喵萌奶茶屋','天月搬运组','萝莉社活动室','千夏生活向上委员会','酷漫404','拨雪寻春','霜庭云花Sub','FSD炸鸽社','雪飘工作室','丸子家族','驯兽师联盟','肥猫压制','离谱','虹咲学园烤肉同好会','AQUA工作室','晨曦制作','夜莺家族','Liella!の烧烤摊']
+    FuzzyMatchData = ['字幕','Raws','sub','汉化','搬运','月新番','Airota','Comicat','DMHY','NC-Raws','ANi','LoliHouse','Sakurato','TSDM','LoveEcho','EMe','Sakura','SweetSub','AHU-SUB','VCB-Studio','GM-Team','MingY','cc动漫','推しの子','喵萌奶茶屋','天月搬运组','萝莉社活动室','千夏生活向上委员会','酷漫404','拨雪寻春','霜庭云花Sub','FSD炸鸽社','雪飘工作室','丸子家族','驯兽师联盟','肥猫压制','离谱','虹咲学园烤肉同好会','AQUA工作室','晨曦制作','夜莺家族','Liella!の烧烤摊']
     #精准待去除
-    FirstIntKeyWord = ['仅限港澳台地区']
-
-    FileType = search(r'(.*?\.)',VideoName[::-1],flags=I).group()[::-1] #匹配视频文件格式
+    PreciseMatchData = ['仅限港澳台地区']
+    filename, FileType = path.splitext(VideoName)
+    #FileType = search(r'(.*?\.)',VideoName[::-1],flags=I).group()[::-1] #匹配视频文件格式
     #统一意外字符
     VideoName = sub(r',|，| ','-',VideoName,flags=I) 
     VideoName = sub(r'[^A-Za-z0-9_\s&/\-\u4e00-\u9fa5]','=',VideoName,flags=I)
@@ -21,10 +21,10 @@ def TitleMatch(VideoName):
     #通过剧集截断文件名
     VideoName = sub(r'%s.*'%Episodes,'',VideoName,flags=I)
     #开始去除其他字符
-    for i in range(len(FirstIntKeyWord)):
-        VideoName = sub(r'%s'%FirstIntKeyWord[i],'',VideoName,flags=I)
-    for i in range(len(SubtitleGroupDate)):
-        VideoName = sub(r'=.*?%s.*?='%SubtitleGroupDate[i],'',VideoName,flags=I)
+    for i in range(len(PreciseMatchData)):
+        VideoName = sub(r'%s'%PreciseMatchData[i],'',VideoName,flags=I)
+    for i in range(len(FuzzyMatchData)):
+        VideoName = sub(r'=.*?%s.*?='%FuzzyMatchData[i],'',VideoName,flags=I)
     VideoName = VideoName.replace('=','').replace(' ','').strip('-')
     #匹配剧季
     if ('/' in VideoName) == True: #按'/'进行双语言分类
@@ -55,23 +55,28 @@ def TitleMatch(VideoName):
 
 def GetArgv():#接受参数
     SavePath,VideoName,Star = argv[1],argv[2],argv[3]
-    if Star != '动漫':#筛选'分类'
-        exit()
+    #筛选分类,您可以根据不同的类型设置不同路径
+    #if Star != '动漫':'
+    #   pass
     return SavePath,VideoName
 
 def AutoMv(SavePath,VideoName,Season,Episodes,VideoTrueName,FileType):#整理+重命名
+    a = ['move /y','mkdir','\\'] if name == 'nt' else ['mv','mkdir -p','/']#识别操作系统
+    print(name)
     NewName = f"S{Season}E{Episodes}{FileType}"
-    NewVideoDir = f"{VideoTrueName}/Season_01"
-    system(f'mkdir -p {SavePath}/{NewVideoDir}')
+    NewVideoDir = f"{VideoTrueName}{a[2]}Season_{Season}"
+    system(f'{a[1]} {SavePath}{a[2]}{NewVideoDir}')
     sleep(2)
-    system(f'mv "/{SavePath}/{VideoName}"  "{SavePath}/{NewVideoDir}/{NewName}"')
+    system(f'{a[0]} "{SavePath}{a[2]}{VideoName}"  "{SavePath}{a[2]}{NewVideoDir}{a[2]}{NewName}"')
 
-def Test(test):#测试TitleMatch函数
-    return TitleMatch(test)
-
-if __name__ == "__main__":
+def Test(test=None):#测试TitleMatch函数
+    #return TitleMatch(test)
+    #AutoMv('E:\D\Test','1.mp4','01','01','TRUETEST','.mp4')
+    pass
+if __name__ != "__main__":
    #sleep(15)
    SavePath,VideoName = GetArgv()
    Season,Episodes,VideoTrueName,FileType = TitleMatch(VideoName)
    AutoMv(SavePath,VideoName,Season,Episodes,VideoTrueName,FileType)
-
+else:
+    Test()
