594b9c0800fc7542836e18be263615bdae390209
Abcuders <Abcuders@gmail.com>
2023-05-29 15:34:42 +0800

    <docs/feat/test>更新md&add 大剧集支持(9999)&add test数据屏蔽功能

--- AutoAnimeMv.py
+++ AutoAnimeMv.py
@@ -11,41 +11,43 @@ def AttributesMatch(VideoName):
     Season = '01' #定义初始剧季和剧集为1
     Episodes = '01'
     #匹配待去除
-    #FuzzyMatchData = ['字幕','Raws','sub','汉化','搬运','月新番','Airota','Comicat','DMHY','NC-Raws','ANi','LoliHouse','Sakurato','TSDM','LoveEcho','EMe','Sakura','SweetSub','AHU-SUB','VCB-Studio','GM-Team','MingY','cc动漫','推しの子','喵萌奶茶屋','天月搬运组','萝莉社活动室','千夏生活向上委员会','酷漫404','拨雪寻春','霜庭云花Sub','FSD炸鸽社','雪飘工作室','丸子家族','驯兽师联盟','肥猫压制','离谱','虹咲学园烤肉同好会','AQUA工作室','晨曦制作','夜莺家族','Liella!の烧烤摊']
     FuzzyMatchData = ['月新番']
     #精准待去除
-    PreciseMatchData = ['仅限港澳台地区']
+    PreciseMatchData = ['仅限港澳台地区','1080p','720p']
     FileType = path.splitext(VideoName)[1]
     #FileType = search(r'(.*?\.)',VideoName[::-1],flags=I).group()[::-1] #匹配视频文件格式
     #统一意外字符
     VideoName = sub(r',|，| ','-',VideoName,flags=I) 
-    VideoName = sub(r'[^A-Za-z0-9_\s&/\-\u4e00-\u9fa5]','=',VideoName,flags=I)
-    #匹配剧集
-    try:
-        Episodes = findall(r'[^0-9a-z\u4e00-\u9fa5][0-2]{1}[0-9]{1}[^0-9a-z\u4e00-\u9fa5]',VideoName,flags=I)[0].strip(" =-_eE")
-    except:
-       pass
-    Log(f"2.匹配剧集为{Episodes}")
-    #通过剧集截断文件名
-    VideoName = sub(r'%s.*'%Episodes,'',VideoName,flags=I)
-    Log(f"3.通过剧集截断文件名为{VideoName}")
+    VideoName = sub(r'[^A-Za-z0-9_\s&/\-: :\u4e00-\u9fa5]','=',VideoName,flags=I)
+    #去除日期
+    VideoName = sub(r'\d{4}.\d{2}.\d{2}','',VideoName,flags=I)
     #开始去除其他字符
     for i in range(len(PreciseMatchData)):
         VideoName = sub(r'%s'%PreciseMatchData[i],'',VideoName,flags=I)
     VideoName = sub(r'^=.*?=','',VideoName,flags=I)
     for i in range(len(FuzzyMatchData)):
         VideoName = sub(r'=.*?%s.*?='%FuzzyMatchData[i],'',VideoName,flags=I)
+    #匹配剧集
+    try:
+        Episodes = findall(r'[^0-9a-z\u4e00-\u9fa5][0-9]{1,4}[^0-9a-z\u4e00-\u9fa5]',VideoName,flags=I)[0].strip(" =-_eE")
+        Episodes = f"0{Episodes}" if len(Episodes) == 1 else Episodes
+    except:
+        pass
+    Log(f"2.匹配剧集为{Episodes}")
+    #通过剧集截断文件名
+    VideoName = sub(r'%s.*'%Episodes,'',VideoName,flags=I)
+    Log(f"3.通过剧集截断文件名为{VideoName}")
     VideoName = VideoName.replace('=','').replace(' ','').strip('-')
     Log(f"4.番剧Name为{VideoName}")
     #匹配剧季
     if ('/' in VideoName) == True: #按'/'进行双语言分类
         VideoName = VideoName.split("/", )
-        #print(VideoName[1].replace('-','').isalnum())
         if VideoName[1].replace('-','').isalnum() == True: #双语言(中英)分类匹配英文Name中的剧季
             if search(r'[0-9]{0,1}[0-9]{1}S',VideoName[1][::-1],flags=I) != None :
                 Season = search(r'[0-9]{0,1}[0-9]{1}S',VideoName[1][::-1],flags=I).group(0)[::-1]
                 TrueVideoName = VideoName[1].strip(Season)
                 Season = search(r'[0-9]{0,1}[0-9]{1}S',VideoName[1][::-1],flags=I).group(0)[::-1].strip('Ss')
+                Season = f"0{Season}" if len(Season) == 1 else Season
                 Log(f"5-1.TrueVideoName={TrueVideoName},Season={Season}")
             else:
                 TrueVideoName = VideoName[1]
@@ -54,19 +56,15 @@ def AttributesMatch(VideoName):
             TrueVideoName = VideoName.strip(Season)
             Season = search(r'(季.*?第|[0-9]{0,1}[0-9]{1}S)',VideoName[::-1],flags=I).group(0)[::-1].strip('第季Ss')
             if Season.isdigit() == True :
+                Season = f"0{Season}" if len(Season) == 1 else Season
                 Log(f"5-2.TrueVideoName={TrueVideoName},Season={Season}")
             else:#中文剧季转化
                 digit = {'一':'01', '二':'02', '三':'03', '四':'04', '五':'05', '六':'06', '七':'07', '八':'08', '九':'09'}
                 Season = digit[Season]
                 Log(f"5-3.TrueVideoName={TrueVideoName},Season={Season}")
     else:
-        TrueVideoName = VideoName
-    try: 
         TrueVideoName = TrueVideoName.strip('-')
-    except:
-        pass
-    print(TrueVideoName,Season)
-    Log(f"5-4.TrueVideoName={TrueVideoName},Season={Season}")
+        Log(f"5-4.TrueVideoName={TrueVideoName},Season={Season}")
     return Season,Episodes,TrueVideoName,FileType
 
 def GetArgv():#接受参数
--- Test.py
+++ Test.py
@@ -1,15 +1,14 @@
+#!/usr/bin/python3
+#coding:utf-8
 from AutoAnimeMv import AttributesMatch
 from ast import literal_eval
 
-def Log(mess):
-    #print(mess)
-    pass
 def Test():
     print(f"现在进入Test mode,正在read test文件")
     with open(f'test','r',encoding='utf-8') as ff:
         for TestData in ff.readlines():
             TestData = TestData.strip()
-            if TestData != None:
+            if TestData != None and TestData[0] != '#':
                 TestData = literal_eval(TestData.strip())
                 Season,Episodes,TrueVideoName,FileType = AttributesMatch(TestData['Bt'])
                 print(Season,Episodes,TrueVideoName,FileType)
@@ -17,5 +16,7 @@ def Test():
                     print(f'{TestData}....Ok\n ')
                 else:
                     print(f'{TestData}....No\n')
+            elif TestData[0] == '#':
+                pass
 if __name__ == '__main__':
     Test()
